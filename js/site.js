23ece80d1d01ed33a0b2232e973ea12f76cfa556
Giancarlo Tzquierdo <gtzquierdo@protonmail.com>
2021-08-10 20:53:03 -0400

    JS completed

--- js/site.js
+++ js/site.js
@@ -1,15 +1,40 @@
 // Get the string from the page
+// Controller Function
 function getValue() {
 
+    document.getElementById("alert").classList.add("invisible");
+
+    let userString = document.getElementById("userString").value;
+    let revString = reverseString(userString);
+    displayString(revString);
 }
 
 // Reverse the string
-function reverseString() {
+// Logic Function
+function reverseString(userString) {
+
+    let revString = [];
+
+    // Reverse the string using a for loop
+    for (let i = userString.length - 1; i >= 0; i--) {
+        
+        revString += userString[i];
+    }
+
+    return revString;
 
 }
 
 
 // Display the reversed string to the page
-function displayString() {
-    
+// View Function
+function displayString(revString) {
+
+    // Write the message to the page
+    document.getElementById("msg").innerHTML = `Your string reversed is: ${revString}`;
+
+    // Turn on the alert box
+    document.getElementById("alert").classList.remove("invisible");
+
 }
+
