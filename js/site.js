224b1a088c6ddf8657fc4265562566ae7902171a
Giancarlo Tzquierdo <gtzquierdo@protonmail.com>
2021-08-10 20:28:11 -0400

    Bases for site.js
    Functions built out. bare bones

---
+++ js/site.js
@@ -0,0 +1,15 @@
+// Get the string from the page
+function getValue() {
+
+}
+
+// Reverse the string
+function reverseString() {
+
+}
+
+
+// Display the reversed string to the page
+function displayString() {
+    
+}
